d903b12228c82a310b7bc3ff5a901900597d5b4f
Reza Maulana <rezamaulana@MacBook-Pro-Reza.local>
2021-07-11 18:44:52 +0700

    init

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http,{
+    cors:true,
+    origins:["http://172.16.4.247:8000"],
+});
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
+io.on('connection', (socket) => {
+    socket.on("pushNotification",(msg)=>{
+        console.log("from web to android ",msg)
+        socket.broadcast.emit("androidPushNotification",msg)
+    })
+    console.log("user has connect")
+});
+io.on("disconnect",(socket)=>{
+    console.log("asd")
+})
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
